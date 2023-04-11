9739c6e69ed093847e4b72a75dadc7191bb5d229
josileudo <josileudorodrigues75@gmail.com>
2023-04-11 19:26:49 -0300

    :bug: fix: Resolved running issues

---
+++ src/app/components/task/index.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  template:`
+    <div>
+      <input type="text" placeholder="test"/>
+    </div>
+  `,
+  styles: [`
+  `]
+})
+
+export default class TaskComponent {}
